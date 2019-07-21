8b5f25dd4580431fdf8524e978b7a2f2e8448f36
starkend <danstarken@gmail.com>
2019-07-21 12:01:22 -0500

    Added port to currencies call to validate Ribbon Load Balancing is working.

--- currency-service/src/main/java/com/starkend/currencyservice/controller/CurrencyController.java
+++ currency-service/src/main/java/com/starkend/currencyservice/controller/CurrencyController.java
@@ -1,14 +1,21 @@
 package com.starkend.currencyservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/currencies")
     public String getCurrencies() {
-        return "Here is a currency list";
+        String port = environment.getProperty("local.server.port");
+
+        return "Here is a currency list on port: " + port;
     }
 
 }
