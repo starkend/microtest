efd168fd6139b4a1ba9d4b353b0fedce5f57357f
starkend <danstarken@gmail.com>
2019-07-21 11:39:19 -0500

    Refactored Ribbon client

--- currency-service-loadbalancer/src/main/java/com/starkend/currencyserviceloadbalancer/CurrencyServiceProxy.java
+++ currency-service-loadbalancer/src/main/java/com/starkend/currencyserviceloadbalancer/CurrencyServiceProxy.java
@@ -1,14 +1,26 @@
 package com.starkend.currencyserviceloadbalancer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@RibbonClient(name="currency-service")
 @FeignClient(name="currency-service")
-@RibbonClient(name ="currency-service")
-public interface CurrencyServiceProxy {
+@RestController
+public class CurrencyServiceProxy {
 
-    @GetMapping("/currencies")
-    public String getCurrencies();
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/currency-call")
+    public String getCurrencies() {
+        String returnString = this.restTemplate.getForObject(
+                "http://currency-service/currencies", String.class);
+
+        return returnString + " success";
+    }
 
 }
